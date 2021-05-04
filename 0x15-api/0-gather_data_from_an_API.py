c2921de24c5ebd6f7090ea0a32d82a6527c1ecec
stivenz <1806@holbertonschool.com>
2021-05-03 20:06:19 -0500

    Fixed space

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
                 NUMBER_OF_DONE_TASKS += 1
 
     print("Employee {} is done with\
- tasks({}/{}):".format(EMPLOYEE_NAME,
+tasks({}/{}):".format(EMPLOYEE_NAME,
                        NUMBER_OF_DONE_TASKS,
                        TOTAL_NUMBER_OF_TASKS))
     for task in request_decoded:
